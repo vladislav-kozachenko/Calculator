a2d01c8f2e273c464d28bee741b143777fe3f356
Vladislav Kozachenko <vladislav.kozachenko@teamdev.local>
2017-11-24 12:46:12 +0200

    Fixed problem with slf4j classes loading.

--- fsm/src/main/java/javaclasses/calculator/fsm/FiniteStateMachine.java
+++ fsm/src/main/java/javaclasses/calculator/fsm/FiniteStateMachine.java
@@ -1,9 +1,10 @@
 package javaclasses.calculator.fsm;
 
-import java.util.Optional;
-import java.util.Set;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.Optional;
+import java.util.Set;
 
 public abstract class FiniteStateMachine<
         Output,
